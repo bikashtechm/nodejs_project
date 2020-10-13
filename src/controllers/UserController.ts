8778b2fe13dbec5cb99e2c5e99a621726afe6442
Bikash Shaw <1985.bikash.shaw@gmail.com>
2020-10-13 14:32:59 +0530

    errorHandling

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,6 +1,13 @@
+import { nextTick } from "process";
+
 export class UserController {
 
-    static login( req, res) {
-        res.send('login called...')
+    static login( req, res, next) {
+        const error = new Error('User does not exists');
+        next(error);
+    }
+
+    static test( req, res, next) {
+        console.log('Called Test Function..')
     }
 }
--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,8 @@ export class Server {
     constructor() {
         this.setConfiguration();
         this.setRoutes();
+        this.error404Handler();
+        this.handleErrors();
         
     }
 
@@ -28,5 +30,26 @@ export class Server {
             .then(() => {
                 console.log('mongoDB is connected...');
             });
+    }
+
+    error404Handler() {
+        this.app.use((req, res) => {
+            res.status(404).json({
+                message: 'Not Found',
+                status_code: 404
+            })
+        });
+    }
+
+    handleErrors() {
+        this.app.use((error, req, res, next) => {
+            const errorStatus = req.errorStatus || 500;
+            res.status(errorStatus).json({
+                message: error.message || 'Something went wrong.Please try again',
+                status_code: errorStatus                
+            })
+            
+        });
+        
     }
 }
